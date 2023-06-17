5faa634085cece8f384fafe9455e521269429bfd
viktoriamay <alterkelly@mail.ru>
2023-06-17 16:00:44 +0300

    Add close selects function by click on document

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useDispatch } from 'react-redux';
 import './App.scss';
+import { CronData } from './components/CronData/CronData';
 import { Schedule } from './components/Schedule/Schedule';
+import { useEffect } from 'react';
+import { changeMonthsVisibility } from './storage/actions/monthsActions';
+import { changeHoursVisibility } from './storage/actions/hoursActions';
+import { changeWeekdayVisibility } from './storage/actions/weekdaysActions';
+import { changeMonthdaysVisibility } from './storage/actions/monthdaysActions';
 
 function App() {
+  const dispatch = useDispatch();
+  
+  const handleOutsideClick = () => {
+    dispatch(changeMonthsVisibility(false));
+    dispatch(changeHoursVisibility(false));
+    dispatch(changeWeekdayVisibility(false));
+    dispatch(changeMonthdaysVisibility(false));
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleOutsideClick);
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
+
   return (
     <div className="App">
       <Schedule />
+      <CronData />
     </div>
   );
 }
