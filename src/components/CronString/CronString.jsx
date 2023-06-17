a49102c1a8247fdf7e5fb05e927803d11c8b03f9
viktoriamay <alterkelly@mail.ru>
2023-06-17 16:05:08 +0300

    Refactor CronString

--- src/components/CronString/CronString.jsx
+++ src/components/CronString/CronString.jsx
@@ -1,67 +1,41 @@
-import { useSelector } from 'react-redux';
-import { useRange } from '../../hooks/useRange';
+import { useCronString } from '../../hooks/useCronString';
+import { CronData } from '../CronData/CronData';
+import './CronString.scss';
 
 export const CronString = () => {
-  const cronInterval = (interval, minute) => {
-    if (minute && interval) {
-      return `*/${interval}`;
-    } else if (minute) {
-      return minute;
-    } else if (interval > 0) {
-      return `*/${interval}`;
-    } else {
-      return '*';
-    }
-  };
-
-  const minutesIntervalValue = useSelector(
-    (state) => +state.interval.minutesIntervalValue
-  );
-  const hoursIntervalValue = useSelector(
-    (state) => +state.interval.hoursIntervalValue
-  );
-  const weekdaysIntervalValue = useSelector(
-    (state) => +state.interval.weekdaysIntervalValue
-  );
-  const monthdaysIntervalValue = useSelector(
-    (state) => +state.interval.monthdaysIntervalValue
-  );
-  const monthsIntervalValue = useSelector(
-    (state) => +state.interval.monthsIntervalValue
-  );
-
-  const minutesValue = useSelector(
-    (state) => state.minutesOptions.selectedMinutes
-  );
-  const hoursValue = useSelector((state) => state.hoursOptions.selectedHours);
-  const weekdaysValue = useSelector(
-    (state) => state.weekdayOptions.selectedWeekdays
-  );
-  const monthdaysValue = useSelector(
-    (state) => state.monthdaysOptions.selectedMonthdays
-  );
-  const monthsValue = useSelector(
-    (state) => state.monthsOptions.selectedMonths
-  );
-
-  const minutesCron = useRange(minutesValue);
-  const hoursCron = useRange(hoursValue);
-  const monthdaysCron = useRange(monthdaysValue);
-  const monthsCron = useRange(monthsValue);
-  const weekdaysCron = useRange(weekdaysValue);
-
-  const cronString = [
-    cronInterval(minutesIntervalValue, minutesCron),
-    cronInterval(hoursIntervalValue, hoursCron),
-    cronInterval(monthdaysIntervalValue, monthdaysCron),
-    cronInterval(monthsIntervalValue, monthsCron),
-    cronInterval(weekdaysIntervalValue, weekdaysCron),
-  ].join(' ');
+  const {
+    minutesCronValue,
+    hoursCronValue,
+    monthdaysCronValue,
+    monthsCronValue,
+    weekdaysCronValue,
+    cronString,
+  } = useCronString();
 
   return (
-    <div>
-      <h2>CronString</h2>
-      <div>{cronString}</div>
+    <div className="cronstring">
+      <div className="cronstring__item">
+        <span className="cronstring__value">{minutesCronValue}</span>
+        <span className="cronstring__name">Minutes</span>
+      </div>
+      <div className="cronstring__item">
+        <span className="cronstring__value">{hoursCronValue}</span>
+        <span className="cronstring__name">Hours</span>
+      </div>
+      <div className="cronstring__item">
+        <span className="cronstring__value">{monthdaysCronValue}</span>
+        <span className="cronstring__name">Days</span>
+      </div>
+      <div className="cronstring__item">
+        <span className="cronstring__value">{monthsCronValue}</span>
+        <span className="cronstring__name">Months</span>
+      </div>
+      <div className="cronstring__item">
+        <span className="cronstring__value">{weekdaysCronValue}</span>
+        <span className="cronstring__name">Weekdays</span>
+      </div>
+
+      {/* <CronData /> */}
     </div>
   );
 };
