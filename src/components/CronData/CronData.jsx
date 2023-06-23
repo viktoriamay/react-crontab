222b83dcadf2f6d06a6091ff9984362b4cdf9c3e
viktoriamay <alterkelly@mail.ru>
2023-06-23 16:29:58 +0300

    Add intervalScheduleChange function

--- src/components/CronData/CronData.jsx
+++ src/components/CronData/CronData.jsx
@@ -3,7 +3,7 @@ import { useCronString } from '../../hooks/useCronString';
 import { useCopyToClipboard } from 'react-use';
 import './CronData.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeMinutesIntervalValue } from '../../storage/actions/intervalActions';
+import { changeHoursIntervalValue, changeMinutesIntervalValue, changeMonthdaysIntervalValue, changeMonthsIntervalValue, changeWeekdaysIntervalValue } from '../../storage/actions/intervalActions';
 import { changeMinutesValue } from '../../storage/actions/minutesActions';
 import { minutes } from '../../data/data';
 
@@ -31,7 +31,7 @@ export const CronData = () => {
   const inputStringMonths = inputData.split(' ')[3];
   const inputStringWeekdays = inputData.split(' ')[4];
 
-  console.log({selectedMinutes});
+  // console.log({selectedMinutes});
 
   // const str = "1-4,6,8";
   const selectedMinutesInInput = [];
@@ -73,67 +73,78 @@ export const CronData = () => {
 
   const uniqueMinutes = [];
 
-  console.log(minutesIds[0]);
+  // console.log(minutesIds[0]);
 
-const d = (inputStringMinutes) => {
+  const d = (inputStringMinutes) => {
+    if (inputStringMinutes === '*') {
+      return;
+    }
 
-  if (inputStringMinutes === '*') {
-    return
-  }
+    const minutesPartComma = inputStringMinutes.split(',');
+    for (let i = 0; i < minutesPartComma.length; i++) {
+      const minutesPartDash = minutesPartComma[i].split('-');
 
-  const minutesPartComma = inputStringMinutes.split(',');
-  for (let i = 0; i < minutesPartComma.length; i++) {
-    const minutesPartDash = minutesPartComma[i].split('-');
-    
-    if (minutesPartDash.length === 2) {
-      
-      const startPartDash = parseInt(minutesPartDash[0]);
-      const endPartDash = parseInt(minutesPartDash[1]);
+      if (minutesPartDash.length === 2) {
+        const startPartDash = parseInt(minutesPartDash[0]);
+        const endPartDash = parseInt(minutesPartDash[1]);
 
-      if (!startPartDash && (endPartDash * -1 < minutesIds[0])) {
-        alert('no')
-      }
-      for (let j = startPartDash; j <= endPartDash; j++) {
-        const minute = minutes.find((m) => m.id === j);
-        if (!minute) {
+        if (!startPartDash && endPartDash * -1 < minutesIds[0]) {
+          alert('no');
+        }
+        for (let j = startPartDash; j <= endPartDash; j++) {
+          const minute = minutes.find((m) => m.id === j);
+          if (!minute) {
+            alert('Введите число от 0 до 59');
+            return;
+          }
+          if (uniqueMinutes.includes(j)) {
+            alert('Значения минут должны быть уникальными');
+            return;
+          }
+          uniqueMinutes.push(j);
+          if (minute) {
+            selectedMinutesInInput.push(minute);
+          }
+        }
+      } else {
+        const num = parseInt(minutesPartComma[i]);
+
+        const minute = minutes.find((m) => m.id === num);
+        if (minute || inputStringMinutes === '*' || inputStringMinutes === '') {
+          selectedMinutesInInput.push(
+            minute || inputStringMinutes === '*' || inputStringMinutes === ''
+          );
+        } else if (!minute) {
           alert('Введите число от 0 до 59');
           return;
         }
-        if (uniqueMinutes.includes(j)) {
+        if (uniqueMinutes.includes(num)) {
           alert('Значения минут должны быть уникальными');
           return;
         }
-        uniqueMinutes.push(j);
-        if (minute) {
-          selectedMinutesInInput.push(minute);
-        }
+        uniqueMinutes.push(num);
       }
-    } else {
-      const num = parseInt(minutesPartComma[i]);
-      
-      const minute = minutes.find((m) => m.id === num);
-      if (minute || inputStringMinutes === '*' || inputStringMinutes === '' ) {
-        selectedMinutesInInput.push(minute || inputStringMinutes === '*' || inputStringMinutes === '');
-      }
-      else if (!minute) {
-        alert('Введите число от 0 до 59');
-        return;
-      }
-      if (uniqueMinutes.includes(num)) {
-        alert('Значения минут должны быть уникальными');
-        return;
-      }
-      uniqueMinutes.push(num);
     }
-  }
-};
+  };
 
- 
+  const intervalScheduleChange = (inputStringValue, dispatchChangeValue) => {
+    if (inputStringValue.includes('/')) {
+    const first = inputStringValue.split('/')[0];
+    const last = +inputStringValue.split('/')[1];
+    // и если это число больше нуля (тк нельзя ввести отрицательный интервал) и первое значение содержит * то мы диспатчим значение минутсИнтервал
+    if (last > 0 && first === '*') {
+      dispatch(dispatchChangeValue(last));
+      setInputData('');
+      //если нет, то выводим алерт и не позволяем отправить данные
+    } else {
+      alert('invalid format');
+    }
+  };}
 
   const handleScheduleLoad = () => {
     // если первый элемент массива (разделенной строки), в данном случае число введенное в инпут (минуты) содержит */, то
-    if (inputStringMinutes.includes('/')) {
-      // получаем первое значение и последнее
+    // if (inputStringMinutes.includes('/')) {
+      /* // получаем первое значение и последнее
       const first = inputStringMinutes.split('/')[0];
       const last = +inputStringMinutes.split('/')[1];
       // и если это число больше нуля (тк нельзя ввести отрицательный интервал) и первое значение содержит * то мы диспатчим значение минутсИнтервал
@@ -143,19 +154,27 @@ const d = (inputStringMinutes) => {
         //если нет, то выводим алерт и не позволяем отправить данные
       } else {
         alert('invalid format');
-      }
+      } */
+
+      intervalScheduleChange(inputStringMinutes, changeMinutesIntervalValue)
+      intervalScheduleChange(inputStringHours, changeHoursIntervalValue)
+      intervalScheduleChange(inputStringDays, changeMonthdaysIntervalValue)
+      intervalScheduleChange(inputStringMonths, changeMonthsIntervalValue)
+      intervalScheduleChange(inputStringWeekdays, changeWeekdaysIntervalValue)
+
+
       // в ином случае работаем с форматом минут
-    }
+    // }
     // else if (!inputStringMinutes.indexOf('*/') + 1) {
     // alert('invalid format');
     //
     // }
-    else {
-  d(inputStringMinutes);
+    /* else {
+      d(inputStringMinutes);
 
       dispatch(changeMinutesValue(selectedMinutesInInput));
       setInputData('');
-    }
+    } */
     // setInputData(cronString);
   };
 
